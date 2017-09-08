a4f764c59c9f2ededc29de727deea66290f39afd
Priyankabhoomraogari <31738755+Priyankabhoomraogari@users.noreply.github.com>
2017-09-08 16:11:10 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
 public class Main {
 
 	public static void main(String[] args) {
+		 Scanner s=new Scanner(System.in);
+		int n; 
+		int id;
+		 String fname;
+		int marks;
+		Date dob;
+		System.out.println("Enter no.of students");
+		n=s.nextInt();
+		s.nextLine();
+		ArrayList studentArray=new ArrayList(n);
+		for(int i=0;i<n;i++)
+		{
+			System.out.println("Enter name");
+			name=s.nextLine();
+			System.out.println("Enter id");
+			id=s.nextInt();
+			System.out.println("Enter marks");
+			marks=s.nextInt();
+			System.out.println("Enter dob");
+			dob=s.nextLine();
+		}
+		StudentArray.add(new Student(name,id,marks,dob));
+
+
+
+			
+		
+		
 		
 		//You may test that your code works find here
 		//Please check that your code works and has no 
